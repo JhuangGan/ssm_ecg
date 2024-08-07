6c88344413ad4107ce7d4d90eccee319ab6b4209
JhuangGan <55838396+JhuangGan@users.noreply.github.com>
2024-08-07 19:05:56 +0800

    Update train_ecg_model.py

--- code/train_ecg_model.py
+++ code/train_ecg_model.py
@@ -252,15 +252,15 @@ def cat(res, key):
 
 
 def evaluate_macro(preds, targets, trainer, idmap):
-    import pandas as pd
     # for val sanity check TODO find cleaner solution
     idmap = idmap[: preds.shape[0]]
     lbl_itos = trainer.datamodule.lbl_itos
     scores = eval_scores(targets, preds, classes=lbl_itos, parallel=True)
     preds_agg, targs_agg = aggregate_predictions(preds, targets, idmap)
     
-    df_pred_targs = pd.DataFrame({'preds_agg':preds_agg, 'targs_agg':targs_agg})
-    df_pred_targs.to_csv('./df_pred_targs.csv')
+    dic = {'preds_agg': preds_agg, 'targs-agg':targs_agg}
+    torch.save(dic, './preds_targs_agg.pth') 
+    
     scores_agg = eval_scores(targs_agg, preds_agg,
                              classes=lbl_itos, parallel=True)
     macro = scores["label_AUC"]["macro"]
