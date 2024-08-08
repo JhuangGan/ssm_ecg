cb21048c16cb9b29e4d4847784103034e4141135
JhuangGan <JhuangGan@163.com>
2024-08-08 09:31:56 +0800

    eval.py train_ecg_model.py

--- eval.py
+++ eval.py
@@ -4,11 +4,11 @@ from sklearn.metrics import roc_auc_score, average_precision_score, f1_score
 def print_info(info=''):
   dic = torch.load('./'+str(info)+'preds_targs_agg'+'.pth')
   
-  macro_auc = np.round(roc_auc_score(dic['targs-agg'], dic['preds_agg'], average='macro'), 3)
-  macro_aupr = np.round(average_precision_score(dic['targs-agg'], dic['preds_agg'], average='macro'), 3)
+  macro_auc = np.round(roc_auc_score(dic['targs_agg'], dic['preds_agg'], average='macro'), 3)
+  macro_aupr = np.round(average_precision_score(dic['targs_agg'], dic['preds_agg'], average='macro'), 3)
   # macro_f1 = np.round(f1_score(dic['targs-agg'], dic['preds_agg'], average='macro'), 3)
   
-  print(f'{valortest} {sig},auc:{macro_auc}, aupr:{macro_aupr}')
+  print(f'{info},auc:{macro_auc}, aupr:{macro_aupr}')
 
 print_info(info='val_')
 print_info(info='val_sig_')
