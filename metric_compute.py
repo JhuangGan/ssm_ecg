33e4329d3e5069ef4ea3cd6d1f1d9eb16ae3455d
JhuangGan <JhuangGan@163.com>
2024-08-08 10:48:50 +0800

    eval.py train_ecg_model.py

--- metric_compute.py
+++ metric_compute.py
@@ -2,6 +2,8 @@
 from sklearn.metrics import precision_recall_curve
 import numpy as np
 import torch
+from argparse import ArgumentParser
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 
 def bestf1score(label, predict):
@@ -14,12 +16,18 @@ def bestf1score(label, predict):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--preds_targs_path", type=int, default=100)
+    parser.add_argument("--preds_targs_path", type=str, default='')
+    parser.add_argument("--info", type=str, default='')
 
-    dic = torch.load('./val_preds_targs.pth')
+    args = parser.parse_args()
+
+    dic = torch.load('./'+str(args.preds_targs_path)+'.pth')
     preds = dic['preds']
     targs = dic['targs']
 
     best_f1_score, thresholds = bestf1score(targs, preds)
-    print()
+
+    macro_auc = np.round(roc_auc_score(dic['targs_agg'], dic['preds_agg'], average='macro'), 3)
+    macro_aupr = np.round(average_precision_score(dic['targs_agg'], dic['preds_agg'], average='macro'), 3)
+    print(f'info:{args.info},f1:{best_f1_score},auc:{macro_auc}, aupr:{macro_aupr}')
 
