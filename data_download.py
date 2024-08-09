f861566e33cfbc27f3797ff4b6746569030d81c3
JhuangGan <JhuangGan@163.com>
2024-08-09 18:04:41 +0800

    little

--- data_download.py
+++ data_download.py
@@ -24,23 +24,28 @@ def download(data_url, dataset_dir, flatten=True):
     # os.remove(filename)
 
 if __name__ == '__main__':
-    target_fs=100
-    data_root=Path("./data/")
-    target_root=Path("./data")
-    if not isdir(data_root):
-        os.makedirs(data_root)
-    if not isdir(target_root):
-        os.makedirs(target_root)
+
+    def ptb_xl_data_download(fs):
+        target_fs=fs
+        data_root=Path("./data/")
+        target_root=Path("./data")
+        if not isdir(data_root):
+            os.makedirs(data_root)
+        if not isdir(target_root):
+            os.makedirs(target_root)
+        
+        data_folder_ptb_xl = data_root/"ptb_xl/" # 数据存放文件夹
+        target_folder_ptb_xl = target_root/("ptb_xl_fs"+str(target_fs))  # 
+        # ptb_xl_url='https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip'
+        # 直接替换链接就可以
+        ptb_xl_url='https://physionet.org/static/published-projects/ptb-xl/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip'
+        
+        download(ptb_xl_url, data_folder_ptb_xl)
+        
+        df_ptb_xl, lbl_itos_ptb_xl,  mean_ptb_xl, std_ptb_xl = prepare_data_ptb_xl(data_folder_ptb_xl, min_cnt=1, target_fs=target_fs, 
+                                                                            channels=12, channel_stoi=channel_stoi_default, 
+                                                                            target_folder=target_folder_ptb_xl, recreate_data=True)
+        reformat_as_memmap(df_ptb_xl, target_folder_ptb_xl/("memmap.npy"),data_folder=target_folder_ptb_xl,delete_npys=False)
     
-    data_folder_ptb_xl = data_root/"ptb_xl/" # 数据存放文件夹
-    target_folder_ptb_xl = target_root/("ptb_xl_fs"+str(target_fs))  # 
-    # ptb_xl_url='https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip'
-    # 直接替换链接就可以
-    ptb_xl_url='https://physionet.org/static/published-projects/ptb-xl/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip'
-    
-    download(ptb_xl_url, data_folder_ptb_xl)
-    
-    df_ptb_xl, lbl_itos_ptb_xl,  mean_ptb_xl, std_ptb_xl = prepare_data_ptb_xl(data_folder_ptb_xl, min_cnt=1, target_fs=target_fs, 
-                                                                           channels=12, channel_stoi=channel_stoi_default, 
-                                                                           target_folder=target_folder_ptb_xl, recreate_data=True)
-    reformat_as_memmap(df_ptb_xl, target_folder_ptb_xl/("memmap.npy"),data_folder=target_folder_ptb_xl,delete_npys=False)
+    ptb_xl_data_download(fs=100)
+    ptb_xl_data_download(fs=500)
