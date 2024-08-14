f3c98a9b0660c5e7f12dfaf87c907727eac2602a
JhuangGan <JhuangGan@163.com>
2024-08-14 11:03:33 +0800

    s

--- code/train_ecg_model.py
+++ code/train_ecg_model.py
@@ -500,7 +500,7 @@ def cli_main():
         loss_fn=nn.CrossEntropyLoss(
         ) if args.binary_classification else F.binary_cross_entropy_with_logits,
         use_meta_information_in_head=args.use_meta_information_in_head,
-        dataset_name=args.dataset,
+        dataset_name=args.target_folder,
         input_size_name=args.input_size,
     )
     # configure trainer
