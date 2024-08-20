98196a08896352d0ef792485b74bef48712c6bd0
JhuangGan <JhuangGan@163.com>
2024-08-20 09:56:08 +0800

    s

--- code/finetuning.py
+++ code/finetuning.py
@@ -687,11 +687,11 @@ def train_model(model, train_loader, valid_loader, test_loader, epochs, loss_fn,
         print("aggregated macro:", macro_agg)
         
         if head_only:
-            torch.save(model.state_dict(), os.path.join(save_model_at,'{}_head_only_model'.format(epoch)))
-            print('save epoch model at', os.path.join(save_model_at,'{}_head_only_model'.format(epoch)))
+            torch.save(model.state_dict(), os.path.join(os.path.dirname(save_model_at),'{}_head_only_model.pt'.format(epoch)))
+            print('save epoch model at', os.path.join(os.path.dirname(save_model_at),'{}_head_only_model.pt'.format(epoch)))
         else:
-            torch.save(model.state_dict(), os.path.join(save_model_at,'after_head_{}_model'.format(epoch)))
-            print('save epoch model at', os.path.join(save_model_at,'after_head_{}_model'.format(epoch)))
+            torch.save(model.state_dict(), os.path.join(os.path.dirname(save_model_at),'after_head_{}_model.pt'.format(epoch)))
+            print('save epoch model at', os.path.join(os.path.dirname(save_model_at),'after_head_{}_model.pt'.format(epoch)))
         if macro_agg > best_macro_agg:
             print('save model at', save_model_at)
             torch.save(model.state_dict(), save_model_at)
