76f1739ffb48d8ffc3a4322c3998bf10957d79ab
JhuangGan <JhuangGan@163.com>
2024-08-20 11:29:02 +0800

    s

--- code/finetuning.py
+++ code/finetuning.py
@@ -1007,7 +1007,7 @@ def run():
     train_mode = 'lin' if args.f_epochs == 0 else "fin"
     
     save_model_at = os.path.join(os.path.dirname(
-            args.model_file), "n=" + str(args.noise_level) + "_"+tag + train_mode +'_'+args.label_class+'_input_size'+str(args.input_size)+"_finetuned") if args.save_model_at is None else args.save_model_at
+            args.model_file), "n=" + str(args.noise_level) + "_"+tag + train_mode +'_'+args.label_class+'_input_size'+str(args.input_size)+'_'+str(args.target_folder)+"_finetuned") if args.save_model_at is None else args.save_model_at
     
     filename = os.path.join(os.path.dirname(
         save_model_at), "t=" + args.trial_nr+"_n=" + str(args.noise_level) + "_"+tag + "res_" + train_mode +".pkl")
