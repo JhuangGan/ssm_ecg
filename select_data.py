d53cc9676578b5cc67988a688ce464efa4aeed79
JhuangGan <JhuangGan@163.com>
2024-08-22 09:11:50 +0800

    s

--- select_data.py
+++ select_data.py
@@ -1,18 +1,19 @@
 import torch
 
-filename = "ptb_xl_fs100_250_label_allpreds_targs"
-
-test_dic = torch.load('./test_'+str(filename)+'.pth')
-test_preds = test_dic['preds']
-test_targs = test_dic['targs']
-
-n_select =9
-test_preds = test_dic['preds'][:, test_dic['targs'].sum(axis=0)>=n_select]
-test_targs = test_dic['targs'][:, test_dic['targs'].sum(axis=0)>=n_select]
+def select(filename, n_select):
+    test_dic = torch.load('./'+str(filename)+'.pth')
+    test_preds = test_dic['preds']
+    test_targs = test_dic['targs']
 
 
-test_dic['preds'] = test_preds
-test_dic['targs'] = test_targs
+    test_preds = test_dic['preds'][:, test_dic['targs'].sum(axis=0)>=n_select]
+    test_targs = test_dic['targs'][:, test_dic['targs'].sum(axis=0)>=n_select]
 
-torch.save(test_dic, './test_ptb_xl_fs100_250_label_allpreds_targs.pth')
 
+    test_dic['preds'] = test_preds
+    test_dic['targs'] = test_targs
+
+    torch.save(test_dic, './'+filename+'_select_'+str(n_select)+'_.pth')
+
+filename1 = "ptb_xl_fs100_250_label_allpreds_targs"
+select(filename1, n_select=9)
