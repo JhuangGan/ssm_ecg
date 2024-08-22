9c33a81d689868305ce4d8c3371e79f85eceb698
JhuangGan <JhuangGan@163.com>
2024-08-22 09:09:31 +0800

    s

--- select_data.py
+++ select_data.py
@@ -6,6 +6,13 @@ test_dic = torch.load('./test_'+str(filename)+'.pth')
 test_preds = test_dic['preds']
 test_targs = test_dic['targs']
 
+n_select =9
+test_preds = test_dic['preds'][:, test_dic['targs'].sum(axis=0)>=n_select]
+test_targs = test_dic['targs'][:, test_dic['targs'].sum(axis=0)>=n_select]
 
 
+test_dic['preds'] = test_preds
+test_dic['targs'] = test_targs
+
+torch.save(test_dic, './test_ptb_xl_fs100_250_label_allpreds_targs.pth')
 
